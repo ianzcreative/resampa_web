ea69dea985496aa0d3c938851925e4bac2173d9b
ianzcreative <ianzcreatives@gmail.com>
2022-02-01 20:00:21 +0700

    update func daftar_transaksi_sampah

--- rest_resampah_nasabah.php
+++ rest_resampah_nasabah.php
@@ -242,7 +242,7 @@ header('Content-Type: application/json');
       } 
 
      $resultArr = array();
-      $result = $connect->query("SELECT tbl_transaksi_sampah.id_transaksi_sampah, tbl_transaksi_sampah.subtotal_transaksi_sampah, tbl_transaksi_sampah.tgl_transaksi_sampah, tbl_transaksi_sampah.catatan_transaksi_sampah, tbl_transaksi_sampah.id_nasabah, SUM(tbl_item_transaksi_sampah.jumlah_item_transaksi) AS jm_items FROM tbl_transaksi_sampah \n"
+      $result = $connect->query("SELECT tbl_transaksi_sampah.id_transaksi_sampah, tbl_transaksi_sampah.subtotal_transaksi_sampah, tbl_transaksi_sampah.tgl_transaksi_sampah, tbl_transaksi_sampah.catatan_transaksi_sampah, tbl_transaksi_sampah.id_nasabah, SUM(tbl_item_transaksi_sampah.jumlah_item_transaksi) AS qty_items FROM tbl_transaksi_sampah \n"
 
     . "JOIN tbl_nasabah ON tbl_transaksi_sampah.id_nasabah = tbl_nasabah.id_nasabah JOIN tbl_item_transaksi_sampah ON tbl_transaksi_sampah.id_transaksi_sampah = tbl_item_transaksi_sampah.id_transaksi_sampah WHERE tbl_nasabah.nik_nasabah ='".$nik."' GROUP BY id_transaksi_sampah ORDER BY id_transaksi_sampah DESC"); 
    //  $result = $conn->query($query); 
@@ -251,7 +251,7 @@ header('Content-Type: application/json');
         if ($result->num_rows > 0 && $_GET["key"] == $key) {
             $resultArr = array('success' => true, 'total' => $result->num_rows);
             while($row = $result->fetch_assoc()) {
-                $resultArr['data_transaksi'][$row['id_transaksi_sampah']] = array('id_transaksi_sampah' => $row['id_transaksi_sampah'], 'subtotal_transaksi_sampah' => $row['subtotal_transaksi_sampah'], 'tgl_transaksi_sampah' => $row['tgl_transaksi_sampah'], 'catatan_transaksi_sampah' => $row['catatan_transaksi_sampah'], 'id_nasabah' => $row['id_nasabah']);
+                $resultArr['data_transaksi'][$row['id_transaksi_sampah']] = array('id_transaksi_sampah' => $row['id_transaksi_sampah'], 'subtotal_transaksi_sampah' => $row['subtotal_transaksi_sampah'], 'tgl_transaksi_sampah' => $row['tgl_transaksi_sampah'], 'catatan_transaksi_sampah' => $row['catatan_transaksi_sampah'], 'id_nasabah' => $row['id_nasabah'], 'qty' => $row['qty_items']);
 
                 //Anwser table results
                 $sql2 = "SELECT tbl_sampah.nama_sampah, tbl_sampah.harga_sampah, tbl_kategori_sampah.nama_kategori_sampah, tbl_item_transaksi_sampah.jumlah_item_transaksi FROM tbl_item_transaksi_sampah JOIN tbl_sampah ON tbl_item_transaksi_sampah.id_sampah = tbl_sampah.id_sampah JOIN tbl_kategori_sampah ON tbl_kategori_sampah.id_kategori_sampah = tbl_sampah.id_kategori_sampah WHERE tbl_item_transaksi_sampah.id_transaksi_sampah = '".$row['id_transaksi_sampah']."'";
