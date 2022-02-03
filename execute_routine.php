79e520fa9538f14af2dca45f90f5538dd94a7e86
ianzcreative <ianzcreatives@gmail.com>
2022-02-03 20:42:33 +0700

    update 3 feb 2022

---
+++ execute_routine.php
@@ -0,0 +1,35 @@
+<?php
+require_once "koneksi_resampah.php";
+header('Content-Type: application/json');
+
+   if(function_exists($_GET['function'])) {
+         $_GET['function']();
+      }
+      
+
+   function exec_iuran_seribu()
+   {
+      global $connect;      
+      $key = "12H6383H3";
+      
+      
+      $query = $connect->query("UPDATE tbl_saldo SET jml_saldo_nasabah = jml_saldo_nasabah - 1000");            
+      while($row=mysqli_fetch_object($query)) {
+         $data[] =$row;
+      }
+      
+      if($data && $_GET["key"] == $key) {
+          
+        $response = array(
+                     'message' =>'Success'
+                  );
+      } else {
+          
+        $response = array(
+                     'message' =>'No Data Found'
+                  );
+      }
+      header('Content-Type: application/json');
+      echo json_encode($response);
+   }
+ ?>
