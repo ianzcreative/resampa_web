f1f494c0bc46c4f39d4797544b876bc93c8f3f10
ianzcreative <ianzcreatives@gmail.com>
2022-02-10 14:48:51 +0700

    update 10 feb 2022

--- rest_resampah_nasabah.php
+++ rest_resampah_nasabah.php
@@ -433,28 +433,23 @@ header('Content-Type: application/json');
       global $connect;      
       $key = "12H6383H3";
       
-      
-      $query = $connect->query("SELECT tbl_profil.id_profil, tbl_profil.subtitle_profil, tbl_profil.icon_profil, tbl_profil.intent_profil, tbl_kategori_profil.kategori_profil AS kategori FROM tbl_profil LEFT JOIN  tbl_kategori_profil ON tbl_profil.id_kategori_profil = tbl_kategori_profil.id_kategori_profil");            
-      while($row=mysqli_fetch_object($query)) {
-         $data[] =$row;
-      }
-      
-      if($data && $_GET["key"] == $key) {
-          
-        $response = array(
-                     'status' => 1,
-                     'message' =>'Success',
-                     'data' => $data
-                  );
-      } else {
-          
-        $response = array(
-                     'status' => 0,
-                     'message' =>'No Data Found'
-                  );
-      }
-      header('Content-Type: application/json');
-      echo json_encode($response);
+      if ($result->num_rows > 0 && $_GET["key"] == $key) {
+            $resultArr = array('success' => true, 'total' => $result->num_rows);
+            while($row = $result->fetch_assoc()) {
+                $resultArr['data_kategori'][$row['id_kategori_profil']] = array('id_kategori_profil' => $row['id_kategori_profil'], 'kategori_profil' => $row['kategori_profil']);
+				
+                $sql2 = "SELECT tbl_profil.id_profil, tbl_profil.subtitle_profil, tbl_profil.icon_profil, tbl_profil.intent_profil, tbl_kategori_profil.kategori_profil AS kategori FROM tbl_profil LEFT JOIN  tbl_kategori_profil ON tbl_profil.id_kategori_profil = tbl_kategori_profil.id_kategori_profil WHERE tbl_profil.id_kategori_profil = '".$row['id_kategori_profil']."'";
+                $result2 = $connect->query($sql2);
+                while($row2 = $result2->fetch_assoc()) {
+                    $resultArr['data_kategori'][$row['id_kategori_profil']]['data'][] = $row2;
+                }
+            }
+            $resultArr['data_transaksi'] = array_values($resultArr['data_transaksi']);
+        } else {
+            $resultArr = array('success' => false, 'total' => 0);
+        }
+	    header('Content-Type: application/json');
+        echo json_encode($resultArr);
    }
       
    function get_provinsi()
